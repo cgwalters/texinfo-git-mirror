b88dd6335978cadb6a74d0b38ab5f6d1e2e04442
pertusus <pertusus>
2003-02-12 12:47:46 +0000

    * Tests/Makefile.am, Tests/test.sh: add regenerate target to         redo the reference files         add test with tidy, remove cvs/rcs tags when doing diffs

--- texi2html/Tests/test.sh
+++ texi2html/Tests/test.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
 
 #set -x
+do_tidy='no'
+which tidy 2>&1 > /dev/null && do_tidy='yes'
 
 test_texi(){
 dir=$1; shift
 texi_file=$1 ;shift
 options=$1; shift
 fail=$1; shift
+ignore_tags=$1; shift
+test_tidy=$1;shift
 wc=$1; shift
 [ -z $fail ] && fail='success'
 [ -z $wc ] && wc=0
 [ -z $texi_file ] && texi_file=$dir.texi
+[ -z $ignore_tags ] && ignore_tags=no
+[ -z $test_tidy ] && test_tidy=yes
+[ $test_tidy = test_tidy -o $test_tidy = tidy ] && test_tidy=yes
+[ $ignore_tags = 'yes' -o $ignore_tags = 'ignore_tags' ] && ignore_tags=yes
 echo "making test: $dir/$texi_file"
 if [ ! -f $dir/$texi_file ]; then
 	echo "  !!! no file $dir/$texi_file"
@@ -18,34 +26,53 @@ if [ ! -f $dir/$texi_file ]; then
 fi
 (cd $dir && perl -w ../../texi2html -test $options $texi_file) 2>res.2 > /dev/null
 ret=$?
-echo "status:"
-if [ $ret = 0 -a $fail = 'fail' ]; then echo "  !!! no failing";
-elif [ $ret != 0 -a $fail = 'success' ]; then echo "  !!! no success";
-else echo "  passed"
+echo "  status:"
+if [ $ret = 0 -a $fail = 'fail' ]; then echo "    !!! no failing";
+elif [ $ret != 0 -a $fail = 'success' ]; then echo "    !!! no success";
+else echo "    passed"
 fi
 
 if [ $wc != 'no' ]; then
-echo "stderr line count:"
+echo "  stderr line count:"
 res_wc=`<res.2 wc -l`
-if [ $res_wc != $wc ]; then echo "  !!! bad line count: $res_wc != $wc"
-else echo "  passed"
+if [ $res_wc != $wc ]; then echo "    !!! bad line count: $res_wc != $wc"
+else echo "    passed"
 fi
 fi
 
 dir_res=${dir}_res
 [ -d $dir_res ] || return
-echo "diffs:"
+echo "  diffs:"
 previous_good='no'
 for file in `ls $dir_res` ; do
+	found='no'
 	if [ -d $dir_res/$file -a $dir_res/$file = $dir_res/'CVS' ]; then continue
 	elif [ -d $dir_res/$file ]; then
-		diff --recursive $dir_res/$file $dir/$file 2>&1 > /dev/null
-		result=$?
 		file_or_dir=dir
+		if [ ! -d $dir/$file ]; then
+			result=1
+		else
+			found='yes'
+			diff --recursive $dir_res/$file $dir/$file 2>&1 > /dev/null
+			result=$?
+		fi
 	elif [ -f $dir_res/$file ]; then
-		diff $dir_res/$file $dir/$file 2>&1 > /dev/null
-		result=$?
 		file_or_dir=file
+		if [ ! -f $dir/$file ]; then
+			result=1
+		else
+			found='yes'
+			if [ $ignore_tags = 'yes' ]; then
+				temp_file=$dir/${file}_tempnotag
+				sed 's/\$\([[:alpha:]]\+\):.*\$/\$\1\$/g' $dir/${file} > $temp_file
+				sed 's/\$\([[:alpha:]]\+\):.*\$/\$\1\$/g' $dir_res/$file | diff - $temp_file 2>&1 > /dev/null
+				result=$?
+				rm $temp_file
+			else			
+				diff $dir_res/$file $dir/$file 2>&1 > /dev/null
+				result=$?
+			fi
+		fi
 	else
 		echo "Unkown file type for $file ???"
 		continue
@@ -54,16 +81,29 @@ for file in `ls $dir_res` ; do
 	if [ $result != 0 ]; then 
 		if [ "$previous_good" = yes ]; then echo
 		fi
-		echo "  !!! $file_or_dir differ: $file";
-		previous_good=no
+		if [ $found = 'no' ]; then
+			echo "    !!! $file_or_dir not found: $file";
+		else
+			echo "    !!! $file_or_dir differ: $file";
+			previous_good=no
+		fi
 	else
 		if [ "$previous_good" = yes ]; then  echo -n "."
-		else echo -n "  ."
+		else echo -n "    ."
 		fi
 		previous_good=yes
 	fi
 done
 if [ "$previous_good" = yes ]; then echo; fi
+
+if [ $test_tidy = 'yes' -a $do_tidy = 'yes' ]; then
+	echo "  test validity with tidy:"
+	if tidy -q -e -f /dev/null $dir/*.html; then
+		 echo "    passed"
+	else echo "    !!! tidy failed"
+	fi
+fi
+	
 }
 
 test_texi GermanNodeTest nodetest.texi
@@ -72,8 +112,8 @@ test_texi texi2html
 test_texi viper_monolithic viper.texi
 test_texi viper viper.texi "-split chapter"
 test_texi xemacs xemacs.texi "-split chapter"
-test_texi texinfo texinfo.txi "-split chapter"
-test_texi ccvs ccvs.texinfo "-split chapter"
+test_texi texinfo texinfo.txi "-split chapter" success ignore_tags
+test_texi ccvs cvs.texinfo "-split chapter"
 
 exit
 #examples of syntax
