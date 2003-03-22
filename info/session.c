b8a049f2fa2b2dbc62418216ff962b417c493321
karl <karl>
2003-03-22 17:34:38 +0000

    (MIN) [MIN]: #undef first if already #define'd.

--- info/session.c
+++ info/session.c
@@ -1,5 +1,5 @@
 /* session.c -- user windowing interface to Info.
-   $Id: session.c,v 1.6 2003/03/06 23:23:42 karl Exp $
+   $Id: session.c,v 1.7 2003/03/22 17:34:38 karl Exp $
 
    Copyright (C) 1993, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003
    Free Software Foundation, Inc.
@@ -2026,6 +2026,9 @@ DECLARE_INFO_COMMAND (info_menu_digit, _("Select this menu item"))
    limit of 200 chars (in Emacs 21.2).  */
 
 /* We don't need anything fancy.  */
+#ifdef MIN
+#undef MIN
+#endif
 #define MIN(a,b) ((a) < (b) ? (a) : (b))
 
 static REFERENCE **
