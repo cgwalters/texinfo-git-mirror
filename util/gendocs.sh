5df0f33bde93e9e226f9fa410e449dbe62d0f3b7
karl <karl>
2003-11-04 13:45:56 +0000

    .

--- util/gendocs.sh
+++ util/gendocs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # gendocs.sh -- generate a GNU manual in many formats.  This script is
 #   mentioned in maintain.texi.  See the help message below for usage details.
-# $Id: gendocs.sh,v 1.5 2003/11/02 19:14:41 karl Exp $
+# $Id: gendocs.sh,v 1.6 2003/11/04 13:45:56 karl Exp $
 # 
 # Copyright (C) 2003 Free Software Foundation, Inc.
 #
@@ -29,7 +29,13 @@ srcdir=`pwd`
 scripturl="http://savannah.gnu.org/cgi-bin/viewcvs/texinfo/texinfo/util/gendocs.sh"
 templateurl="http://savannah.gnu.org/cgi-bin/viewcvs/texinfo/texinfo/util/gendocs_template"
 
-rcs_revision='$Revision: 1.5 $'
+: ${MAKEINFO="makeinfo"}
+: ${TEXI2DVI="texi2dvi"}
+: ${DVIPS="dvips"}
+: ${GENDOCS_TEMPLATE_DIR="."}
+unset CDPATH
+
+rcs_revision='$Revision: 1.6 $'
 rcs_version=`set - $rcs_revision; echo $2`
 program=`echo $0 | sed -e 's!.*/!!'`
 version="gendocs.sh $rcs_version
@@ -64,8 +70,8 @@ http://www.gnu.org/prep/maintain_toc.html
 MANUAL-TITLE is included as part of the HTML <title> of the overall
 manual/index.html file.  It should include the name of the package being
 documented.  manual/index.html is created by substitution from the file
-$GENDOCS_TEMPLATE_DIR/gendocs_template; you can modify this generic
-version for your own purposes, if you like.
+$GENDOCS_TEMPLATE_DIR/gendocs_template.  (Feel free to modify the
+generic template for your own purposes.)
 
 If you have several manuals, you'll need to run this script several
 times with different YOURMANUAL values, specifying a different output
@@ -75,13 +81,9 @@ with links to them all.
 You can set the environment variables MAKEINFO, TEXI2DVI, and DVIPS to
 control the programs that get executed, and GENDOCS_TEMPLATE_DIR to
 control where the gendocs_template file is looked for.
-"
 
-: ${MAKEINFO="makeinfo"}
-: ${TEXI2DVI="texi2dvi"}
-: ${DVIPS="dvips"}
-: ${GENDOCS_TEMPLATE_DIR="."}
-unset CDPATH
+Email bug reports or enhancement requests to bug-texinfo@gnu.org.
+"
 
 calcsize()
 {
