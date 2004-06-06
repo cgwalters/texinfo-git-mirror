f30f9975fe9f1ebc642da135009848d44c919275
karl <karl>
2004-06-06 14:04:12 +0000

    (add_char): don't call html_output_head if only_macro_expansion (e.g., a macro call in the Top menu).  From: Yuri D'Elia <wavexx@yuv.info>, 05 Jun 2004 21:11:00 +0200.

--- makeinfo/makeinfo.c
+++ makeinfo/makeinfo.c
@@ -1,5 +1,5 @@
 /* makeinfo -- convert Texinfo source into other formats.
-   $Id: makeinfo.c,v 1.59 2004/04/29 12:00:19 karl Exp $
+   $Id: makeinfo.c,v 1.60 2004/06/06 14:04:12 karl Exp $
 
    Copyright (C) 1987, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
    2000, 2001, 2002, 2003, 2004 Free Software Foundation, Inc.
@@ -2612,6 +2612,7 @@ add_char (int character)
            any order and with any omissions, and we'll still output
            the html <head> `just in time'.  */
         if ((executing_macro || !executing_string)
+            && !only_macro_expansion
             && html && !html_output_head_p && !defining_copying ())
           html_output_head ();
 
