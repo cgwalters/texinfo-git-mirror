9252d825f0685f8fd03ada399d3e495150853349
karl <karl>
2007-06-17 23:12:00 +0000

    (painfully) update language list

--- makeinfo/lang.h
+++ makeinfo/lang.h
@@ -1,7 +1,8 @@
 /* lang.h -- declarations for language codes etc.
-   $Id: lang.h,v 1.10 2007/01/07 19:11:37 karl Exp $
+   $Id: lang.h,v 1.11 2007/06/17 23:12:00 karl Exp $
 
-   Copyright (C) 1999, 2001, 2002, 2003, 2006, 2007 Free Software Foundation, Inc.
+   Copyright (C) 1999, 2001, 2002, 2003, 2006, 2007 Free Software
+   Foundation, Inc.
 
    This program is free software; you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -14,8 +15,8 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program; if not, write to the Free Software
-   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
    Originally written by Karl Heinz Marbaise <kama@hippo.fido.de>.  */
 
@@ -27,33 +28,18 @@
  * These code are the ISO-639 two letter codes.
  */
 typedef enum
-{
-  aa,  ab,  af,  am,  ar,  as,  ay,  az,
-  ba,  be,  bg,  bh,  bi,  bn,  bo,  br,
-  ca,  co,  cs,  cy,
-  da,  de,  dz,
-  el,  en,  eo,  es,  et,  eu,
-  fa,  fi,  fj,  fo,  fr,  fy,
-  ga,  gd,  gl,  gn,  gu,
-  ha,  he,  hi,  hr,  hu,  hy,
-  ia,  id,  ie,  ik,  is,  it,  iu,
-  ja,  jw,
-  ka,  kk,  kl,  km,  kn,  ko,  ks,  ku,  ky,
-  la,  ln,  lo,  lt,  lv,
-  mg,  mi,  mk,  ml,  mn,  mo,  mr,  ms,  mt,  my,
-  na,  ne,  nl,  no,
-  oc,  om,  or,
-  pa,  pl,  ps,  pt,
-  qu,
-  rm,  rn,  ro,  ru,  rw,
-  sa,  sd,  sg,  sh,  si,  sk,  sl,  sm,  sn,  so,  sq,  sr,  ss,  st,  su,  sv,  sw,
-  ta,  te,  tg,  th,  ti,  tk,  tl,  tn,  to,  tr,  ts,  tt,  tw,
-  ug,  uk,  ur,  uz,
-  vi,  vo,
-  wo,
-  xh,
-  yi,  yo,
-  za,  zh,  zu,
+{ 
+  aa, ab, ae, af, ak, am, an, ar, as, av, ay, az, ba, be, bg, bh, bi,
+  bm, bn, bo, br, bs, ca, ce, ch, co, cr, cs, cu, cv, cy, da, de, dv,
+  dz, ee, el, en, eo, es, et, eu, fa, ff, fi, fj, fo, fr, fy, ga, gd,
+  gl, gn, gu, gv, ha, he, hi, ho, hr, ht, hu, hy, hz, ia, id, ie, ig,
+  ii, ik, io, is, it, iu, ja, jv, ka, kg, ki, kj, kk, kl, km, kn, ko,
+  kr, ks, ku, kv, kw, ky, la, lb, lg, li, ln, lo, lt, lu, lv, mg, mh,
+  mi, mk, ml, mn, mo, mr, ms, mt, my, na, nb, nd, ne, ng, nl, nn, no,
+  nr, nv, ny, oc, oj, om, or, os, pa, pi, pl, ps, pt, qu, rm, rn, ro,
+  ru, rw, sa, sc, sd, se, sg, si, sk, sl, sm, sn, so, sq, sr, ss, st,
+  su, sv, sw, ta, te, tg, th, ti, tk, tl, tn, to, tr, ts, tt, tw, ty,
+  ug, uk, ur, uz, ve, vi, vo, wa, wo, xh, yi, yo, za, zh, zu,
   last_language_code
 } language_code_type;
 
