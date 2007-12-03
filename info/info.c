36a5247333de1c0385d9301d936ef7f06d7583d4
karl <karl>
2007-12-03 01:49:07 +0000

    keep multibyte locales at least for testing

--- info/info.c
+++ info/info.c
@@ -1,5 +1,5 @@
 /* info.c -- Display nodes of Info files in multiple windows.
-   $Id: info.c,v 1.22 2007/11/16 23:44:41 karl Exp $
+   $Id: info.c,v 1.23 2007/12/03 01:49:07 karl Exp $
 
    Copyright (C) 1993, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003,
    2004, 2005, 2007 Free Software Foundation, Inc.
@@ -153,19 +153,6 @@ main (int argc, char *argv[])
 #ifdef HAVE_SETLOCALE
   /* Set locale via LC_ALL.  */
   setlocale (LC_ALL, "");
-
-  /* But don't use translated messages in the case when
-     string width and length can differ */
-  if (MB_CUR_MAX > 1)
-    {
-      setenv("LANGUAGE", "C", 1);
-      setenv("LANG", "C", 1);
-#ifdef LC_MESSAGES
-      setlocale (LC_MESSAGES, "C");
-#endif
-      setenv("LC_CTYPE", "C", 1);
-      setenv("LC_ALL", "C", 1);
-    }
 #endif /* HAVE_SETLOCALE */
 
 #ifdef ENABLE_NLS
