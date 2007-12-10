5711d20b7d948b9021279f0e108f821ba6445941
karl <karl>
2007-12-10 00:55:10 +0000

    improve help message

--- makeinfo/makeinfo.c
+++ makeinfo/makeinfo.c
@@ -1,5 +1,5 @@
 /* makeinfo -- convert Texinfo source into other formats.
-   $Id: makeinfo.c,v 1.113 2007/12/03 01:38:43 karl Exp $
+   $Id: makeinfo.c,v 1.114 2007/12/10 00:55:10 karl Exp $
 
    Copyright (C) 1987, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999,
    2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007
@@ -376,16 +376,16 @@ Output format selection (default is to produce Info):\n\
 
     puts (_("\
 General output options:\n\
-  -E, --macro-expand FILE     output macro-expanded source to FILE.\n\
-                              ignoring any @setfilename.\n\
+  -E, --macro-expand=FILE     output macro-expanded source to FILE,\n\
+                                ignoring any @setfilename.\n\
       --no-headers            suppress node separators, Node: lines, and menus\n\
                                 from Info output (thus producing plain text)\n\
                                 or from HTML (thus producing shorter output);\n\
                                 also, write to standard output by default.\n\
-      --no-split              suppress splitting of Info or HTML output,\n\
-                              generate only one output file.\n\
+      --no-split              suppress the splitting of Info or HTML output,\n\
+                                generate only one output file.\n\
       --number-sections       output chapter and sectioning numbers.\n\
-  -o, --output=FILE           output to FILE (directory if split HTML),\n\
+  -o, --output=FILE           output to FILE (or directory if split HTML).\n\
 "));
 
     printf (_("\
