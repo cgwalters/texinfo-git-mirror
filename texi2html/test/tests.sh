f46173df3fec7a519694b879c4cfe67647284f22
pertusus <pertusus>
2009-06-21 09:25:39 +0000

    * texi2html.pl: process @*contents within pass_text.         close previous def_item before calling begin_format_texi, if needed.         * texi2html.pl, formats/info.init: handle @printindex like a normal         @-command.         * formats/info.init: warn for missing file.txt for image only if         there is also no image file.         * tests/coverage/*: add test results for info and raw plaintext.

--- texi2html/test/tests.sh
+++ texi2html/test/tests.sh
@@ -4,6 +4,13 @@ if [ z"$srcdir" = 'z' ]; then
   srcdir=.
 fi
 
+command=run_test.sh
+
+if [ z"$1" = 'zall' ]; then
+  command=run_test_all.sh
+  shift
+fi
+
 arg=
 if [ z"$1" = 'z-clean' ]; then
   arg='-clean'
@@ -13,12 +20,17 @@ elif [ z"$1" = 'z-copy' ]; then
   shift
 fi
 
+if [ z"$1" = 'zall' ]; then
+  command=run_test_all.sh
+  shift
+fi
+
 failed=0
 while [ z"$1" != 'z' ]; do
   dir=$1
   shift
   [ -d "$dir" ] || mkdir $dir
-  (export srcdir_test=$dir; cd "$dir" && ../"$srcdir"/run_test.sh $arg)
+  (export srcdir_test=$dir; cd "$dir" && ../"$srcdir"/"$command" $arg)
   result=$?
   echo "$dir: $result"
   [ $result != 0 ] && failed=1
