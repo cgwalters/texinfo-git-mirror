bd22d3142260aadcd7a8c3a90565e176a9d395c5
pertusus <pertusus>
2009-08-25 10:49:12 +0000

    Add a script that generates the expanded nested_formats manuals for all the formats tested, and try makeinfo on the resulting manuals.

---
+++ texi2html/test/nested_formats/generate_all_texi.pl
@@ -0,0 +1,34 @@
+#! /usr/bin/perl -w
+
+# expand the manuals with nested formats using texi2html and test
+# with makeinfo on these expanded manuals (it is likely that makeinfo 
+# cannot expand those manuals in some cases).
+use strict;
+
+open (TXT, 'tests.txt') or die "Cannot open tests.txt: $!\n";
+
+mkdir ("all_texi");
+
+while (<TXT>)
+{
+   next if /^\s*\#/;
+   if (/^(\w+) nested_formats.texi -D (\w+)$/)
+   {
+      my $dir = $1;
+      my $def = $2;
+      my $format;
+      mkdir ("all_texi/$dir");
+      if ($def =~ /[a-z]+_([a-z]+)$/)
+      {
+         $format = $1;
+      }
+      system("perl -w -x ../../texi2html.pl  nested_formats.texi -D $def --macro-expand=all_texi/$format-nested_formats.texi");
+      system ("cp all_texi/$format-nested_formats.texi all_texi/$dir/nested_formats.texi");
+      #system ("perl -w -x ../../texi2html.pl -D $def --expand-macro=all_texi/$dir/$format-nested_formats.texi");
+      system ("cd all_texi/$dir/ && makeinfo --force nested_formats.texi 2>nested_formats.2");
+   }
+   else
+   {
+       print STDERR "Ignoring $_";
+   }
+}
