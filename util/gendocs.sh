2f79fafbe7250331f13d0bebef87c8dbd5715d45
karl <karl>
2010-01-03 14:31:17 +0000

    .

--- util/gendocs.sh
+++ util/gendocs.sh
@@ -4,7 +4,7 @@
 
 scriptversion=2010-01-02.16
 
-# Copyright 2003, 2004, 2005, 2006, 2007, 2008, 2009
+# Copyright 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010
 # Free Software Foundation, Inc.
 #
 # This program is free software: you can redistribute it and/or modify
