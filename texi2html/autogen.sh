b870984931546840468ade98a2a341bed1b803b2
pertusus <pertusus>
2010-07-01 08:43:39 +0000

    Force installation of autopoint files and create the m4/ directory.

--- texi2html/autogen.sh
+++ texi2html/autogen.sh
@@ -3,12 +3,13 @@
 
 test -f Makefile.am || cd "`dirname \"$0\"`"
 
-autopoint
+autopoint -f
 cp po/* po_messages
 cp po/* po_document
 rm -rf po
 
 # Create `aclocal.m4'.
+mkdir -p m4
 aclocal -I m4
 
 # Create `Makefile.in' from `Makefile.am', and symlink `install-sh',
