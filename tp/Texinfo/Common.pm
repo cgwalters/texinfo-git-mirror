8b6d0c9039376b5b86a7a5b08ed22fd4429885fe
pertusus <pertusus>
2011-08-20 14:38:46 +0000

    Add 2011 in copyright notices.

--- tp/Texinfo/Common.pm
+++ tp/Texinfo/Common.pm
@@ -1,6 +1,6 @@
 # Common.pm: definition of commands. Common code of other Texinfo modules.
 #
-# Copyright 2010 Free Software Foundation, Inc.
+# Copyright 2010, 2011 Free Software Foundation, Inc.
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
--- tp/Texinfo/Structuring.pm
+++ tp/Texinfo/Structuring.pm
@@ -1,7 +1,7 @@
 # Structuring.pm: extract informations about a document structure based on the 
 #                 document tree.
 #
-# Copyright 2010 Free Software Foundation, Inc.
+# Copyright 2010, 2011 Free Software Foundation, Inc.
 # 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
